85f4fd4d399cd65675a79f990e5f7b77b8e04d95
xberto <x.zenteno.a@gmail.com>
2020-03-04 15:52:01 -0300

    log viewerd added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import uuid
-from flask import Flask,redirect, url_for
+from flask import Flask,redirect, url_for, render_template
 from flask_dance.consumer import OAuth2ConsumerBlueprint, oauth_authorized
 from dotenv import load_dotenv
 from pymongo import MongoClient
@@ -26,17 +26,29 @@ app.register_blueprint(gob_digital)
 
 @oauth_authorized.connect
 def logged_in(blueprint, token):
+	#cada vez que alguien inicia sesión, se guarda su información, en mongo, con la fecha del login
 	resp = blueprint.session.post("https://www.claveunica.gob.cl/openid/userinfo")
 	objeto = resp.json()
 	objeto["date"] = datetime.datetime.now()
 	db.logs.insert_one(objeto)
 
+
 @app.route("/")
 def index():
+	#si no está el toke, redirige al prompt, y si es que está... deja pasar.
+	#por mientras muestra los datos del usuario
 	if not gob_digital.session.token:
 		return redirect(url_for('gob_digital.login'))
 	resp = gob_digital.session.post("https://www.claveunica.gob.cl/openid/userinfo")
 	return resp.json()
+
+
+@app.route('/logs')
+def logs():
+	#obtiene los logs de cada inicio de sesion exitoso y los renderiza en una plantilla
+	_logs = db.logs.find().sort("date",-1)
+	return render_template('logs.html',logs=_logs)
+
 
 if __name__ == "__main__":
 	
